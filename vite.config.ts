a0bb5a1602c30f1aeff7ff99e51c33b0898c0a24
Nandha-2004 <vkn.nandhakumar@gmail.com>
2025-08-19 15:02:03 +0530

    build for git

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/amirthan-oil-mills/",
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+  exclude: ['lucide-react'],
   },
 });
+
